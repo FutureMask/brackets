36f3f84c9287946ec90e68051a023594473bd825
FutureMask <maks564698@gmail.com>
2021-03-14 21:10:18 +0300

    init: task solutione

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let open = {
+      
+      '{': '}',
+      '[': ']',
+      '(': ')'
+  };
+
+  let closed = {
+      
+      '}': true,
+      ']': true,
+      ')': true
+  }
+
+  for (let i = 0; i < str.length; i++) {
+
+      let char = str[i];
+
+      if (open[char]) {
+          stack.push(char);
+      } else if (closed[char]) {
+          if (open[stack.pop()] !== char) return false;
+      }
+  }
+  return stack.length === 0;
+  
 }
+ 
+  /*
+  let brackets = "][}{})("
+  let stack = []
+
+  for(let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket)
+  }
+    return true ? true : false
+*/
+  // if (regexp = new RegExp("()", "g"))
+  //  return regexp.test(str, bracketsConfig)
+   // else return false
+    
+ //   else (regexp = new RegExp("() []", "g"))
+    //  return regexp.test(str, bracketsConfig)
+    
+   
